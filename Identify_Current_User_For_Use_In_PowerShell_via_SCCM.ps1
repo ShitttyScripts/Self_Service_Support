bd4ed6e7b518d32ba35605c026c701e4069487cb
ShitttyScripts <52809959+ShitttyScripts@users.noreply.github.com>
2019-07-12 07:10:56 -0400

    Add files via upload

---
+++ Identify_Current_User_For_Use_In_PowerShell_via_SCCM.ps1
@@ -0,0 +1,16 @@
+﻿
+############################################################################################
+# Remove users not accessed in 30 days or over
+# Created by: Greg Knackstedt
+# Date: 7.12.2017
+############################################################################################
+
+#Identify currently logged in user
+$ActiveUser = Get-WmiObject -ComputerName $env:COMPUTERNAME -Class Win32_Computersystem | Select-Object UserName
+
+#Seperate username into $Domain and $CurrentUsername
+$DomainName,$CurrentUsername = $ActiveUser.Username.split('\',2)
+
+#Display output to verify correct user detected
+Write-Host $DomainName
+Write-Host $CurrentUsername
