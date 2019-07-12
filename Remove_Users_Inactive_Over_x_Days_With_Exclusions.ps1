a9637637e2aba29fec8be4571acc1ce56f5d2d2d
ShitttyScripts <52809959+ShitttyScripts@users.noreply.github.com>
2019-07-11 23:56:41 -0400

    Add files via upload

---
+++ Remove_Users_Inactive_Over_x_Days_With_Exclusions.ps1
@@ -0,0 +1,28 @@
+﻿
+############################################################################################
+# Remove users not accessed in 30 days or over
+# Created by: Greg Knackstedt
+# Date: 7.11.2017
+############################################################################################
+#
+# This script will identify and remove all users inactive for 30+ days. Excluding accounts defined using declarations below.
+#
+############################################################################################
+######################### Verified compatable with Powershell 2.0 ##########################
+############################################################################################
+#
+# Set maximum last login date for user profile.
+# Example: 30 = 30 days
+$AccountAgeOver = "30"
+#
+############################################################################################
+#
+# User accounts to exclude
+$ExcludeAccountOne = "C:\Users\username"
+$ExcludeAccountTwo = "C:\Users\username"
+$ExcludeAccountTwo = "C:\Users\username"
+#
+############################################################################################
+
+# Get user profiles where the following are true. It is not a special account, not currently logged in, last login over $AccountAgeOver days ago, and not one of the pre-defined/excluded accounts
+Get-WmiObject Win32_UserProfile | Where-Object { (!$_.Special) -and ($_.Loaded -eq "$LoginState") -and ($_.ConvertToDateTime($_.LastUseTime) -lt ((Get-Date).AddDays($AccountAgeOver))) -and ($_.LocalPath -notcontains "$ExcludeAccountOne") -and ($_.LocalPath -notcontains "$ExcludeAccountTwo") -and ($_.LocalPath -notcontains "$ExcludeAccountThree") } | Remove-WmiObject
